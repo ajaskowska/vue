cbdf9199103617f6e78de51ee4e14dc659ef2251
Anna Jaśkowska <anna@macbook-air-anna-1.home>
2022-11-15 13:42:36 +0100

    basics

--- app.js
+++ app.js
@@ -11,4 +11,84 @@ const app = Vue.createApp({
             return this.age + 5;
         }
     }
-}).mount('#assignment')
+}).mount('#assignment');
+const app2 = Vue.createApp({
+    data() {
+        return {
+            counter: 0,
+            name: ""
+        }
+    },
+    methods: {
+        reduce(num){
+            this.counter= this.counter - num;
+        },
+        setName(event){
+            this.name = event.target.value;
+        },
+        submitForm(event){
+           // event.preventDefault(); --> tę funkcję zastępuje .prevent modifier
+            alert('Submitted')
+
+        },
+        resetInput(){
+            this.name = ''
+        }
+    }
+
+}).mount('#events')
+
+const app3 = Vue.createApp({
+    data() {
+        return {
+            name: "",
+            confirmedName: ''
+        }
+    },
+    methods: {
+        showAlert() {
+            alert("hello!");
+        },
+        registerUser(e){
+            this.name = e.target.value
+        },
+        confirmName (){
+            this.confirmedName = this.name
+        }
+    }
+}).mount('#part3');
+
+const app4 = Vue.createApp({
+    data() {
+        return {
+            number: 0,
+            result:""
+        }
+    },
+    watch:{
+        showResult() {
+            const that = this;
+            setTimeout(function (){
+                that.number = 0
+            }, 5000);
+        }
+    },
+    computed: {
+        showResult(){
+            if (this.number < 37) {
+                return "Not there yet";
+            }
+             if (this.number > 37) {
+                 return "Too much!";
+             }
+            else {return this.number}
+            }
+
+        },
+
+    methods: {
+        add(num){
+            this.number = this.number + num;
+        }
+    }
+}).mount('#part4');
